e3ab4aff9c4b40bc4ce7af3ab48076019a42f634
uijunn <ismailjundi7@gmail.com>
2024-08-27 21:16:25 +0700

    update all section, add image, add interaction

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-let fullName = prompt("Siapakah nama Anda?", "Ismail Akromul Jundi");
+const backTo = document.getElementById('back-to');
 
-document.getElementById("username").innerText = fullName;
+document.addEventListener('DOMContentLoaded', function () {
+    // input nama dengan javascript
+    let fullName = prompt('Siapakah nama Anda?', 'Ismail Akromul Jundi');
+
+    document.getElementById('username').innerText = fullName;
+
+    // mengubah warna navbar ketika discroll
+    window.addEventListener('scroll', navScroll);
+})
+
+function navScroll() {
+    if (window.scrollY > 100) {
+        document.querySelector('.nav-bar').classList.add('solid');
+        backTo.classList.add('appear');
+    } else {
+        document.querySelector('.nav-bar').classList.remove('solid');
+        backTo.classList.remove('appear');
+    }
+}
